30faff82cd85c50a6f1e1c93aaca380f9b374431
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-09-26 19:57:39 +0530

    Done with cart functionalities

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const dotenv = require('dotenv')
-
-
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotanv = require("dotenv");
 const { bgCyan } = require("colors");
 require("colors");
 const connectDb = require("./config/config");
-dotenv.config();
+//dotenv config
+dotanv.config();
+//db config
 connectDb();
+//rest object
 const app = express();
 
+//middlwares
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-app.get("/",(req, res)=>{
-res.send("<h1>DUDUBUBU</h1>");
-});
-
+//routes
 app.use("/api/items", require("./routes/itemRoutes"));
-app.use(cors())
+
+//port
 const PORT = process.env.PORT || 2129;
 
-
+//listen
 app.listen(PORT, () => {
   console.log(`Server Running On Port ${PORT}`.bgCyan.white);
 });
